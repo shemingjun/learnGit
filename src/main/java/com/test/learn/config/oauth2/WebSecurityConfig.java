c5c3099e3fc451d9f4bbc5e4492e8a8f61df581f
Mingjun.She <Mingjun.She@Luxsan-ict.com>
2023-07-05 08:39:45 +0800

    Swagger 配置

--- src/main/java/com/test/learn/config/oauth2/WebSecurityConfig.java
+++ src/main/java/com/test/learn/config/oauth2/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.test.learn.config.oauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,6 +34,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
+
+                // 对于登录login 注册register 验证码captchaImage 允许匿名访问
+                .antMatchers("/login", "/register", "/captchaImage").anonymous()
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/",
+                        "/*.html",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/profile/**"
+                ).permitAll()
+                .antMatchers("/swagger-ui.html").anonymous()
+                .antMatchers("/swagger-resources/**").anonymous()
+                .antMatchers("/webjars/**").anonymous()
+                .antMatchers("/*/api-docs").anonymous()
+                .antMatchers("/druid/**").anonymous()
+
                 .antMatchers("/r/r1").hasAnyAuthority("p1")
                 .antMatchers("/login*").permitAll()
                 .anyRequest().authenticated()
