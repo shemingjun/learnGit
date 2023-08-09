a27bd7e15674c80793e0141657632746efc7c41a
Mingjun.She <Mingjun.She@Luxsan-ict.com>
2023-08-09 13:14:49 +0800

    embracing

--- src/main/java/com/test/learn/config/oauth2/TokenConfig.java
+++ src/main/java/com/test/learn/config/oauth2/TokenConfig.java
@@ -6,7 +6,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
